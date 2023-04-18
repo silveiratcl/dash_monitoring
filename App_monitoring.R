b61213f394f63e4a5ffeac51eb0dd787b8c4bf8d
Thiago Silveira <62719017+silveiratcl@users.noreply.github.com>
2023-04-18 17:21:34 -0300

    edits on code - english version - checkbox enabled

--- App_monitoring.R
+++ App_monitoring.R
@@ -1,8 +1,13 @@
 library(shiny)
+library(shinydashboard)
 library(leaflet)
 library(rgdal)
-library(sp)
+library(maptools)
+library(htmlwidgets)
 library(sf)
+library(sp)
+library(tmap)
+library(dplyr)
 
 
 # Import shapefiles data
@@ -15,59 +20,65 @@ ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-                dateRangeInput("data", "Selecione intervalo de data: ", 
+                dateRangeInput("daterange", "Select date range: ", 
                                format = "yyyy-mm-dd",
                                start = "2022-06-15",
                                end =  "2023-12-31",
-                               separator = " até "),
+                               separator = " to "),
                 
-                # 1 # 
-                checkboxGroupInput("data", label = "Select shapefile:", choices = c("Dafor", "Geomorfologia"))      
-               
-               # 2 #
-                #checkboxGroupInput(
-                 #"shapes", "Select shapefile:",
-                 #choices = c("Dafor" = dafor_shp, "Geomorfologia" = geo_shp$localidade ))
-              
-                #3 
-             #  checkboxInput(inputId = "dafor_shp", #needs work
-              #               label = strong("DAFOR - Coral Sol"),
-               #              value = FALSE),
-               
-              # checkboxInput(inputId = "geo_shp", #needs work
-               #              label = strong("Geomorfologia"),
-                #             value = FALSE)
-            
-          )
+                
+                checkboxGroupInput("layers", 
+                                   label = "Select shapefile:", 
+                                   choices = c("Dafor", "Geomorphology"),
+                                   selected = c("Dafor","Geomorphology")
+                )      
+                
+                
+  )
 )
 
-#https://shiny.rstudio.com/tutorial/written-tutorial/lesson3/
 
 
 # Define Server
-server <- function(input, output, session) {
-  
-  
-  
+server <- function(input, output) {
   
   # Define reactive data
-  reactiveData <- eventReactive(input$data, {
+  reactiveData <- eventReactive(input$daterange, {
+    
     # Filter shapefiles data based on selected date range
-    filtered_dafor <-  dafor_shp[dafor_shp$data >= input$data[1] & dafor_shp$data <= input$data[2], ]
-    filtered_geo <- geo_shp[geo_shp$data >= input$data[1] & geo_shp$data <= input$data[2], ]
-  
+    filtered_dafor <-  dafor_shp[dafor_shp$data >= input$daterange[1] & dafor_shp$data <= input$daterange[2], ]
+    filtered_geo <- geo_shp[geo_shp$data >= input$daterange[1] & geo_shp$data <= input$daterange[2], ]
+    
+    
+    
+    # Extract variables column and add to filtered_dafor
+    localidade_dafor <- dafor_shp$localidade
+    n_divers <- dafor_shp$n_divers
+    n_tr_pr <- dafor_shp$n_tr_pr
+    vis_horiz <- dafor_shp$vis_horiz 
+    comp_m_dafor <- dafor_shp$comp_m
+    
+    
+    
+    # Extract variables column and add to filtered_geo
+    localidade_geo <- geo_shp$localidade
+    iah_seg<- geo_shp$iah_seg
+    comp_m_geo <- geo_shp$comp_m
+    
     
     
     # Return filtered shapefiles data
     list(filtered_dafor = filtered_dafor, 
-         filtered_geo = filtered_geo) ### needs work
-    
-
-    
-    
+         filtered_geo = filtered_geo,
+         localidade_dafor = localidade_dafor,
+         localidade_geo = localidade_geo,
+         n_divers = n_divers,
+         n_tr_pr = n_tr_pr,
+         vis_horiz = vis_horiz,
+         iah_seg = iah_seg,
+         comp_m_dafor = comp_m_dafor,
+         comp_m_geo = comp_m_geo)
   })  
-  
- #https://shiny.rstudio.com/articles/basics.html
   
   
   
@@ -75,37 +86,59 @@ server <- function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      setView(-48.38, -27.28, zoom = 10) %>% 
-      addPolylines(data =reactiveData()$filtered_dafor,
-                  fillColor = "#FF0000",
-                  fillOpacity = 0.5,
-                  color = "#FF0000",
-                  weight = 8,
-                  popup = ~paste0("<strong>Localidade: </strong> ", localidade, "<br>",
-                                  "<strong>Data:</strong> ", data, "<br>",
-                                  "<strong>N. Mergulhadores: </strong> ", n_divers, "<br>", 
-                                  "<strong>N. Transectos presente: </strong> " , n_tr_pr,"<br>",
-                                  "<strong>Distancia(m): </strong> ", comp_m),
-                  labelOptions = labelOptions(noHide = F, direction = "rigth")) %>%
-      
-      addPolylines(data = reactiveData()$filtered_geo,
-                  fillColor = "#0000FF",
-                  fillOpacity = 0.5,
-                  color = "#0000FF",
-                  weight = 8,
-                  popup = ~paste0("<strong>Localidade:</strong> ", localidade, "<br>",
-                                  "<strong>Data:</strong> ", data, "<br>",
-                                  "<strong>IAH:</strong> ", iah_seg, "<br>",
-                                  "<strong>Distancia(m): </strong> ", comp_m),
-                  labelOptions = labelOptions(noHide = F, direction = "right"))
+      setView(-48.38, -27.28, zoom = 10)%>%
+      addLegend(
+        position = "bottomright",
+        colors = c("red", "blue"),
+        labels = c("Dafor", "Geomorphology"),
+        title = "Legend"
+      )
+    #addProviderTiles("CartoDB.Positron")
   })
-}
+  
+  observe({
+    # Show/hide layers based on check box input
+    if ("Dafor" %in% input$layers) {
+      leafletProxy("map", data = reactiveData()$filtered_dafor) %>%
+        addPolylines(fillColor = "red",
+                     fillOpacity = 0.5,
+                     color = "red",
+                     weight = 8,
+                     popup = ~paste0("<strong>Locality: </strong> ", reactiveData()$localidade_dafor, "<br>",
+                                     "<strong>Date:</strong> ", data, "<br>",
+                                     "<strong>N. Divers: </strong> ", reactiveData()$n_divers, "<br>", 
+                                     "<strong>Horizontal Visibility(m): </strong> ", reactiveData()$vis_horiz, "<br>",
+                                     "<strong>N. Transects present: </strong> " , reactiveData()$n_tr_pr,"<br>",
+                                     "<strong>Distance(m): </strong> ", reactiveData()$comp_m_dafor),
+                     labelOptions = labelOptions(noHide = F, direction = "rigth")
+        )
+    } else {
+      leafletProxy("map", data = reactiveData()$filtered_dafor) %>%
+        clearShapes()
+    }
+    
+    
+    if("Geomorphology" %in% input$layers) {
+      leafletProxy("map", data = reactiveData()$filtered_geo) %>%
+        addPolylines(fillColor = "blue",
+                     fillOpacity = 0.5,
+                     color = "blue",
+                     weight = 8,
+                     popup = ~paste0("<strong>Locality: </strong> ", reactiveData()$localidade_geo, "<br>",
+                                     "<strong>Date:</strong> ", data, "<br>",
+                                     "<strong>Segment IAH: </strong> ", reactiveData()$iah_seg, "<br>", 
+                                     "<strong>Distancia(m): </strong> ", reactiveData()$comp_m_geo),
+                     labelOptions = labelOptions(noHide = F, direction = "rigth")
+        )
+    } else {
+      leafletProxy("map", data = reactiveData()$filtered_dafor) %>% 
+        clearShapes()
+    }
+    
+  }
+  )}
 
 # Run the app
 shinyApp(ui = ui, server = server)
-
-
-
-
 
 
--- app17.R
+++ app17.R
@@ -20,17 +20,17 @@ ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-                dateRangeInput("daterange", "Selecione intervalo de data: ", 
+                dateRangeInput("daterange", "Select date range: ", 
                               format = "yyyy-mm-dd",
                                start = "2022-06-15",
                                end =  "2023-12-31",
-                               separator = " até "),
+                               separator = " to "),
                 
                 
                 checkboxGroupInput("layers", 
                                    label = "Select shapefile:", 
-                                   choices = c("Dafor", "Geomorfologia"),
-                                   selected = "Dafor"
+                                   choices = c("Dafor", "Geomorphology"),
+                                   selected = c("Dafor","Geomorphology")
                 )      
                 
                 
@@ -54,6 +54,7 @@ server <- function(input, output) {
     # Extract variables column and add to filtered_dafor
     n_divers <- dafor_shp$n_divers
     n_tr_pr <- dafor_shp$n_tr_pr
+    vis_horiz <- dafor_shp$vis_horiz 
     comp_m_dafor <- dafor_shp$comp_m
     
     
@@ -69,16 +70,25 @@ server <- function(input, output) {
          filtered_geo = filtered_geo,
          n_divers = n_divers,
          n_tr_pr = n_tr_pr,
+         vis_horiz = vis_horiz,
          iah_seg = iah_seg,
          comp_m_dafor = comp_m_dafor,
          comp_m_geo = comp_m_geo)
   })  
   
+
+  
   
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      setView(-48.38, -27.28, zoom = 10) #%>% 
+      setView(-48.38, -27.28, zoom = 10)%>%
+      addLegend(
+        position = "bottomright",
+        colors = c("red", "blue"),
+        labels = c("Dafor", "Geomorphology"),
+        title = "Legend"
+      )
       #addProviderTiles("CartoDB.Positron")
   })
   
@@ -86,15 +96,16 @@ server <- function(input, output) {
     # Show/hide layers based on check box input
     if ("Dafor" %in% input$layers) {
       leafletProxy("map", data = reactiveData()$filtered_dafor) %>%
-        addPolylines(fillColor = "#FF0000",
+        addPolylines(fillColor = "red",
                      fillOpacity = 0.5,
-                     color = "#FF0000",
+                     color = "red",
                      weight = 8,
-                      popup = ~paste0("<strong>Localidade: </strong> ", reactiveData()$localidade, "<br>",
-                                     "<strong>Data:</strong> ", data, "<br>",
-                                    "<strong>N. Mergulhadores: </strong> ", reactiveData()$n_divers, "<br>", 
-                                   "<strong>N. Transectos presente: </strong> " , reactiveData()$n_tr_pr,"<br>",
-                                  "<strong>Distancia(m): </strong> ", reactiveData()$comp_m_dafor),
+                      popup = ~paste0("<strong>Locality: </strong> ", reactiveData()$localidade, "<br>",
+                                     "<strong>Date:</strong> ", data, "<br>",
+                                    "<strong>N. Divers: </strong> ", reactiveData()$n_divers, "<br>", 
+                                    "<strong>Horizontal Visibility(m): </strong> ", reactiveData()$vis_horiz, "<br>",
+                                   "<strong>N. Transects present: </strong> " , reactiveData()$n_tr_pr,"<br>",
+                                  "<strong>Distance(m): </strong> ", reactiveData()$comp_m_dafor),
                      labelOptions = labelOptions(noHide = F, direction = "rigth")
         )
     } else {
@@ -103,16 +114,15 @@ server <- function(input, output) {
     }
     
     
-    if("Geomorfologia" %in% input$layers) {
+    if("Geomorphology" %in% input$layers) {
       leafletProxy("map", data = reactiveData()$filtered_geo) %>%
-        addPolylines(fillColor = "#120001",
+        addPolylines(fillColor = "blue",
                      fillOpacity = 0.5,
-                     color = "#120001",
+                     color = "blue",
                      weight = 8,
-                      popup = ~paste0("<strong>Localidade: </strong> ", reactiveData()$localidade, "<br>",
-                                     "<strong>Data:</strong> ", reactiveData()$data, "<br>",
-                                    "<strong>N. Mergulhadores: </strong> ", reactiveData()$n_divers, "<br>", 
-                                   "<strong>N. Transectos presente: </strong> " , reactiveData()$n_tr_pr,"<br>",
+                      popup = ~paste0("<strong>Locality: </strong> ", reactiveData()$localidade, "<br>",
+                                     "<strong>Date:</strong> ", data, "<br>",
+                                    "<strong>Segment IAH: </strong> ", reactiveData()$iah_seg, "<br>", 
                                   "<strong>Distancia(m): </strong> ", reactiveData()$comp_m_geo),
                      labelOptions = labelOptions(noHide = F, direction = "rigth")
         )
