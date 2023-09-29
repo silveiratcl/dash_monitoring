398e70ab02b43502f9973f1a0575a14fc4de9258
silveiratcl <silveira.tcl@gmail.com>
2023-09-29 17:24:35 -0300

    manejo shp uploaded developing days since last managemment

--- App_monitoring.R
+++ App_monitoring.R
@@ -9,6 +9,7 @@ library(dplyr)
 library(markdown)
 library(leaflet.extras)
 library(mapview)
+library(lubridate)
 
 updateDate <- format(file.info("App_monitoring.R")$mtime, "%d-%m-%Y")
 
@@ -26,6 +27,8 @@ local_shp <- st_read("shp/localidade.shp")
 rebio_shp <- st_read("shp/limite_rebio.shp")
 
 occ_shp <- st_read("shp/manchas_cs.shp")
+
+manejo_shp <- st_read("shp/manejo_cs.shp")
 
 # Create indicators layers
 
@@ -75,6 +78,58 @@ ntrans_mrg_local_shp <- dafor_shp %>%
   st_as_sf(sf_column_name = "geometry.y")
 
 # Days Elapsed since last management
+########################
+########################
+
+#get system date
+# calculate elapsed days bsed on this
+
+days_after_mng_mrg_local_shp <- manejo_shp %>%
+  data.frame() %>%
+  merge(., local_shp, by = "localidade", all.x = TRUE) %>%
+  filter(!st_is_empty(geometry.y)) %>%
+  group_by(localidade) %>%
+  mutate(max_data = max(data)) %>% 
+  mutate(days_since_last_record = as.numeric(max_data - data)) %>%
+  select(-c(geometry.x)) %>% 
+  st_as_sf(sf_column_name = "geometry.y") %>% 
+  print(width=Inf)
+
+
+
+
+
+
+#######  
+  
+# Calculate the maximum date for each locality in manejo_shp
+max_dates <- manejo_shp %>%
+  group_by(localidade) %>%
+  summarize(max_date = max(data))
+
+days_after_mng_mrg_local_shp <- manejo_shp %>%
+  st_join(local_shp) %>%
+  left_join(max_dates, by = c("localidade" = "localidade")) %>%
+  mutate(days_since_last_record = as.numeric(max_data - data)) %>%
+  select(-c(max_data, geometry.x))%>%
+  st_as_sf(sf_column_name = "geometry")
+
+
+  
+# Merge manejo_shp with local_shp using st_join
+days_after_mng_mrg_local_shp <- manejo_shp %>%
+  st_join(local_shp) %>%
+  filter(!st_is_empty(geometry)) %>%
+  left_join(max_dates, by = c("localidade" = "localidade")) %>%
+  mutate(days_since_last_record = as.numeric(max_date - data)) %>%
+  select(-c(max_date, geometry.x)) %>%
+  st_as_sf(sf_column_name = "geometry")  
+
+
+  
+#############################  
+#############################  
+
 
 # Mass managed
 
